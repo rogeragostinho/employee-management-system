0bca68477cbe3a2bb55503811431edc93c3c76fa
Róger Agostinho <rogeragostinho76@gmail.com>
2026-05-18 16:34:37 +0100

    first commit

---
+++ src/test/java/com/rogeragostinho/employeemanagement/EmployeemanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rogeragostinho.employeemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeemanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
